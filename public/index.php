b2db5963b01bf32ea49ed8293b9c3638062037e5
anton <anton.intern@rarus.ru>
2020-12-17 16:13:29 +0300

    init

--- public/index.php
+++ public/index.php
@@ -3,10 +3,16 @@
 declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
-
+include("/etc/php.d/php-scripts.log");
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
+error_reporting(E_ALL);
+ini_set("error_reporting","E_ALL");
+ini_set("error_log", "log.txt"); // расположение лог-файла ошибок
+ini_set("log_errors", "true"); // включить лог ошибок
+error_log( "Hello, errors!" ); // записать в лог-файл значение/строку
+//trigger_error("error",E_USER_ERROR);
 
 $log = new Logger('name');
 $log->pushHandler(new StreamHandler(dirname(__DIR__) . '/logs/webhook.log', Logger::DEBUG));
@@ -16,10 +22,10 @@ $log->pushProcessor(new \Monolog\Processor\IntrospectionProcessor());
 
 print('<pre>');
 var_dump('hello 1111111');
-echo phpinfo();
-ini_set("log_errors", 1); // включить лог ошибок
-ini_set("error_log", "../../php-error.log"); // расположение лог-файла ошибок
-error_log( "Hello, errors!" ); // записать в лог-файл значение/строку
+//var_dump($i = 5/0);
+//echo phpinfo();
+
+
 
 $log->debug(
     'req',
